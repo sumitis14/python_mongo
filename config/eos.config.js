856acac8b193455b13dfb1e755865ce49ae32cbe
Sumit Saurabh <ssaurabh262@gmail.com>
2018-09-28 16:39:15 +0530

    updated readme

---
+++ config/eos.config.js
@@ -0,0 +1,9 @@
+module.exports= {
+    keyProvider:['hidden'],
+    chainId: '038f4b0fc8ff18a4f0842a8f0564611f6e96e8535901dd45e43ac8691a1c4dca',
+    httpEndpoint: 'http://jungle.cryptolions.io:18888',
+    expireInSeconds: 50,
+    broadcast: true,
+    debug: false,
+    sign: true
+}
